c05bc29c12abbc0ce9f2dcb518f6863169bc5c1d
kimhyesom <kimhyesom1202@gmail.com>
2024-02-24 23:07:28 +0900

    ✨ Implement product carousel slider

---
+++ js/productCarousel.js
@@ -0,0 +1,12 @@
+const productCarousel = tns({
+  container: '.product-carousel .slider-list',
+  controls: false,
+  navContainer: '.product-carousel .thumbnail-list',
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  navAsThumbnails: true,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+})
